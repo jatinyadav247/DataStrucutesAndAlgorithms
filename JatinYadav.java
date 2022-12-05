91dcef46673afbdee3a8b138d58a1ba57d2bfcaa
jatinyadav247 <mac@macui-MacBook-Air.local>
2022-12-05 14:27:24 +0530

    added the first file in dataStructureAndAlgorithmRepo

---
+++ JatinYadav.java
@@ -0,0 +1,11 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.math.*;
+
+public class JatinYadav {
+	public static void main(String[] args) {
+		System.out.println("Hi my name is jatin yadav and from today's onward i will be going to practise for data structures and algorithms for product base orfanisations");
+	}
+
+}
